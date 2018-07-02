efbce97c4f9398729182a7f4cd203631e5864fb1
rahulmisra2000 <rahulmisra2000@gmail.com>
2018-07-02 01:38:03 -0400

    initial

---
+++ 05/demos/end/TourManagement/TourManagement.API/Dtos/TourAbstractBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TourManagement.API.Dtos
+{
+    public abstract class TourAbstractBase : IValidatableObject
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Title is required.")]
+        [MaxLength(200, ErrorMessage = "Title is too long.")]
+        public string Title { get; set; }
+
+        [MaxLength(2000, ErrorMessage = "Description is too long.")]
+        public virtual string Description { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "The start date is required.")]
+        public DateTimeOffset StartDate { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "The end date is required.")]
+        public DateTimeOffset EndDate { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!(StartDate < EndDate))
+            {
+                yield return new ValidationResult(
+                "The start date should be smaller than the end date.",
+                new[] { "Tour" });
+            }
+
+        }
+    }
+}
---
+++ 08/demos/start/TourManagement/TourManagement.Client/src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:44353/api/',
+  openIdConnectSettings: {
+    authority: 'https://localhost:44398/',
+    client_id: 'tourmanagementclient',
+    redirect_uri: 'https://localhost:4200/signin-oidc',
+    scope: 'openid profile roles',
+    response_type: 'id_token',
+    post_logout_redirect_uri: 'https://localhost:4200/',
+  }
+};
+
